1db316bd5d49e6ffe365f110475e73215a03fb48
lorinbeer <lorin@adobe.com>
2013-04-09 13:53:13 -0700

    [jiracli] license and pw safety

--- node/apache-jira-cli/apache-jira-cli.js
+++ node/apache-jira-cli/apache-jira-cli.js
@@ -1,3 +1,16 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 var querystring = require('querystring');
 var http = require('http');
@@ -11,14 +24,14 @@ var PresetPostFields = {
     'issuetype' : 2,
     'security' : -1
 };
-//https://jira.atlassian.com/secure/CreateIssueDetails.jspa?X-Atlassian-Token: no-check&pid=10420&issuetype=1&summary=TestIssue is a test issue&description=TestIssue Description
+
 var PresetPostOptions = {
-    host : "jira.atlassian.com",
-    port : 80,
-    path : "/secure/QuickCreateIssue.jspa",
-    method : 'POST',
+    'host' : "jira.atlassian.com",
+    'port' : 80,
+    'path' : "/secure/QuickCreateIssue.jspa",
+    'method' : 'POST',
     'X-Atlassian-Token': 'no-check',
-    Authorization : auth
+    'Authorization' : auth
 }
 /**
  *
