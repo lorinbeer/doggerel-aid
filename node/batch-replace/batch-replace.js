2bdc6c51575eeae80df82f6a2aa876f5ad150a4e
lorinbeer <lorin@adobe.com>
2013-08-19 19:20:30 -0700

    file processer now reads file stream, batch processes files, removed dead code

--- node/batch-replace/batch-replace.js
+++ node/batch-replace/batch-replace.js
@@ -6,30 +6,21 @@ function processDir(dir) {
     fs.readdir(dir, function (err, files) {
         for (var i in files) {
             console.log(files[i]);
-            var readstream = fs.createReadStream("LICENSE", {'start':0, 'end':64, 'encoding':"utf8"});
-            readstream.on('readable', function (fd) {
-                var chunk;
-                while (null != (chunk = readstream.read())) {
-                    console.log(chunk);
-                }
-            });
+            processFile(files[i]);
         }
     });
 }
 
 
 
-function processFile() {
-    //find match
-    console.log("processing file");    
-//console.log(filestream.read(64));
-    
-/*
-    fs.read(file, buffer, 0, 16, 0, function(err, bytesRead, buffer) {
-        console.log(buffer);
+function processFile(fileName) {
+    var readstream = fs.createReadStream(fileName, {'encoding':"utf8"});
+    readstream.on('readable', function (fd) {
+        var chunk;
+        while (null != (chunk = readstream.read())) {
+            console.log(chunk);
+        }
     });
-*/    //adjust position
-    //insert new     
 }
 
 processDir("/Users/lorin/dev/doggerel-aid/node/batch-replace");
